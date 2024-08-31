ca4694cc763c478e06bd8b394f336f0b6c653009
VsEd <eddy.navia.405@gmail.com>
2024-08-31 01:00:49 -0400

    first commit

---
+++ Source/PlataformasSpawn/PlataformasSpawnGameMode.cpp
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlataformasSpawnGameMode.h"
+#include "PlataformasSpawnCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Plataformas.h"
+
+APlataformasSpawnGameMode::APlataformasSpawnGameMode()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	Posicion = FVector(1180.f, -370.f, 140.f);
+	Rotacion = FRotator(0.f, 0.f, 2.f);
+	PosicionAuxiliar = 510.f;
+	Time = 0.f;
+	spawnsPlataformas = 0;
+}
+
+void APlataformasSpawnGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	for (int i = 0; i < 8; i++) {
+		PT = GetWorld()->SpawnActor<APlataformas>(APlataformas::StaticClass(), Posicion, Rotacion);
+		ContPlataformas.Add(PT);
+		Posicion.Z += 400.0f;
+		Posicion.Y += PosicionAuxiliar + FMath::FRandRange(10.f, 25.f);
+		Rotacion.Roll *= -1;
+		PosicionAuxiliar *= -1;
+	}
+
+	spawnsPlataformas = ContPlataformas.Num() - 1; //4 elementos [0,1,2,3]
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, 
+		FString::Printf(TEXT("Cantidad del contenedor: %d"), ContPlataformas.Num()));
+}
+
+void APlataformasSpawnGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	Time += DeltaTime;
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue,
+		FString::Printf(TEXT("Cantidad del contenedor: %d"), ContPlataformas.Num()));
+
+	if (Time > 3.0f) {
+		if (spawnsPlataformas != -1) {
+			AActor* plataforma = ContPlataformas[spawnsPlataformas];
+			plataforma->Destroy();
+			ContPlataformas.RemoveAt(ContPlataformas.Num() - 1);
+			Time = 0.f;
+			spawnsPlataformas -= 1;
+		}
+		
+	}
+	
+}
